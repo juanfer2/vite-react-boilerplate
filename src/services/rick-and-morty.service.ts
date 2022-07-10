99f42f504f60040e9979f299d581848e15f590a0
Juan Fernando Villadiego Madera <64051193+juanfer2@users.noreply.github.com>
2022-07-10 13:43:10 -0500

    Update rick-and-morty.service.ts

--- src/services/rick-and-morty.service.ts
+++ src/services/rick-and-morty.service.ts
@@ -8,7 +8,7 @@ AxiosInterceptor();
 const BASE_URL = 'https://rickandmortyapi.com/api';
 
 export const getCharacters = async (): Promise<Character[] | Error> => {
-  const response: AxiosResponse = await axios.get(`${BASE_URL}/characterasdf`);
+  const response: AxiosResponse = await axios.get(`${BASE_URL}/character`);
 
   return response.data.results;
 };
